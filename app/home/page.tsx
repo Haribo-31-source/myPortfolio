a71f30d3fde34454eaeb7f0257141331e301c4f9
İbrahim Yıldırım <ibrahimyildirim272000@gmail.com>
2025-04-29 14:38:06 +0300

    bitti1

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -6,17 +6,17 @@ export default function Home() {
     <div className="container">
 <main>
         <div className="texts">
-        <h1>Hello , I'm İbrahim</h1>
-        <h4>I'm a design and coding enthusiast who specializes in backend development. While my main focus is backend, I also have a solid understanding of frontend.</h4>
+        <h1>Hello , I&apos;m İbrahim</h1>
+        <h4>I&apos;m a design and coding enthusiast who specializes in backend development. While my main focus is backend, I also have a solid understanding of frontend.</h4>
     </div>
         <div className="frbc">
             <div className="frontEnd">
                 <Image src="/frontend.png" alt="FrontEnd" width={1000} height={1000}></Image>
-                <p>I'm a design and coding enthusiast who specializes in backend development, but I believe great frontend is what brings the soul to an application. I enjoy crafting intuitive, responsive, and visually engaging interfaces that turn ideas into seamless user experiences</p>
+                <p>I&apos;m a design and coding enthusiast who specializes in backend development, but I believe great frontend is what brings the soul to an application. I enjoy crafting intuitive, responsive, and visually engaging interfaces that turn ideas into seamless user experiences</p>
             </div>
             <div className="backend">
             <Image src="/backend.png" alt="BackEnd" width={1000} height={1000}></Image>
-                <p>I'm a design and coding enthusiast with a strong focus on backend development. I love building robust, scalable, and secure systems that power seamless digital experiences behind the scenes.</p>
+                <p>I&apos;m a design and coding enthusiast with a strong focus on backend development. I love building robust, scalable, and secure systems that power seamless digital experiences behind the scenes.</p>
             </div>
         </div>
     </main>
